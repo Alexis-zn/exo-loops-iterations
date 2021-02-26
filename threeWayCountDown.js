5efce04622ce1019ea42e8e279a45d1738f61b2a
Alexis-zn <alexis.zentici@gmail.com>
2021-02-26 16:49:09 +0100

    done

---
+++ threeWayCountDown.js
@@ -0,0 +1,18 @@
+ for(let i = 101; i >= 1 ; i-=2 ) {
+  console.log(i)
+} 
+
+/*
+let i = 101
+do {
+  i-=2
+  console.log(i)
+} while(i > 1)
+
+
+let i = 101
+while(i > 1 ) {
+  i-=2
+  console.log(i)
+}
+*/  
